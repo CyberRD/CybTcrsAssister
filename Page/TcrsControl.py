de11f9847d13e672a85f959089e1600667fcddfb
alanliu <alanliu@cybersoft4u.com>
2016-08-18 19:55:59 +0800

    prepare mapping activity approach

--- Page/TcrsControl.py
+++ Page/TcrsControl.py
@@ -8,32 +8,44 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import time, re
 
+from loc import TcrsControl as loc
+
 _logger = logging.getLogger(__name__)
+
+TCRS_BASE_URL = "https://tcrs.cybersoft4u.com.tw/"
 
 
 class TcrsController(object):
 
+    PAGE_URL = "/TCRS/login.jsp"
+
     def __init__(self):
         self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(30)
-        self.base_url = "https://tcrs.cybersoft4u.com.tw/"
+        self.base_url = TCRS_BASE_URL
         self.verificationErrors = []
         self.accept_next_alert = True
 
     def navigate_to_timecard_page(self, login_name, login_pwd):
         driver = self.driver
-        driver.get(self.base_url + "/TCRS/login.jsp")
-        driver.find_element_by_name("name").clear()
-        driver.find_element_by_name("name").send_keys(login_name)
-        driver.find_element_by_name("pw").clear()
-        driver.find_element_by_name("pw").send_keys(login_pwd)
-        driver.find_element_by_name("Image12").click()
-        driver.find_element_by_link_text("Timecard").click()
 
-    def select_project_and_activity(self, project, activity):
+        driver.get(self.base_url + self.PAGE_URL)
+
+        driver.find_element_by_name(loc.name).clear()
+        driver.find_element_by_name(loc.name).send_keys(login_name)
+        driver.find_element_by_name(loc.password).clear()
+        driver.find_element_by_name(loc.password).send_keys(login_pwd)
+        driver.find_element_by_name(loc.login_btn).click()
+
+        driver.find_element_by_link_text(loc.tab_timecard).click()
+
+    def select_activity(self, activity_name, project_name=None):
         pass
 
-    def select_spent_hours(self, date, activity):
+    def select_spent_hours(self, date, activity_name):
+        pass
+
+    def is_spent_hours_set(self, date, activity_name):
         pass
 
     def save_record(self):
