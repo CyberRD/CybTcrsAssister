5872b0626edb72b41acb9cb12ac6fd30a074655c
alanliu <alanliu@cybersoft4u.com>
2016-08-19 17:39:14 +0800

    build locator info ready

--- Page/loc/TcrsControl.py
+++ Page/loc/TcrsControl.py
@@ -1,13 +1,5 @@
 # -*- coding: utf-8 -*-
 
-
-class Activity(object):
-
-    project_name = ""
-    project_loc = ""
-    text = ""
-
-
 # page login
 name = "name"
 password = "pw"
@@ -15,23 +7,3 @@ login_btn = "Image12"
 
 # page Help
 tab_timecard = "Timecard"
-
-loc_of_project_name = {
-    u"2015CyberMARS產品研發專案": "project2",
-    "2016Y_SDD"               : "project3"
-}
-
-activity = {
-    u"管理事務_如時程,資源規劃與管理等 <<1.2.3>>": u"2015CyberMARS產品研發專案",
-    u"專案進度會議 <<1.2.4>>"                  : u"2015CyberMARS產品研發專案",
-    u"測試環境建置 <<1.5.1>>"                  : u"2015CyberMARS產品研發專案",
-    u"測試系統設定 <<1.5.2>>"                  : u"2015CyberMARS產品研發專案",
-    u"測試文件撰寫 <<1.5.3>>"                  : u"2015CyberMARS產品研發專案",
-    u"測試案例撰寫 <<1.5.4>>"                  : u"2015CyberMARS產品研發專案",
-    u"測試案例執行 <<1.5.5>>"                  : u"2015CyberMARS產品研發專案",
-    u"自動化測試程式或工具準備與撰寫 <<1.5.6>>"  : u"2015CyberMARS產品研發專案",
-    u"Bug重現 <<1.5.7>>"                      : u"2015CyberMARS產品研發專案",
-    u"測試報告撰寫 <<1.5.8>>"                  : u"2015CyberMARS產品研發專案",
-    u"Bug驗證 <<1.5.9>>"                      : u"2015CyberMARS產品研發專案",
-    u"產品文件與相關資料收集 <<1.5.10>>"        : u"2015CyberMARS產品研發專案"
-}
--- ProfileControl/__init__.py
+++ ProfileControl/__init__.py
@@ -2,12 +2,13 @@
 import logging
 from ConfigParser import SafeConfigParser
 
+
 _logger = logging.getLogger(__name__)
 
 
 class ProfileLoader(object):
 
-    def __init__(self):
+    def __init__(self, profile):
         pass
 
 
