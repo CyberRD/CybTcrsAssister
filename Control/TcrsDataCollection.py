ca05a2995eff7edc80467b19fd6074deb4f907e5
alanliu <alanliu@cybersoft4u.com>
2016-09-06 18:10:03 +0800

    finish spent hour decision

--- Control/TcrsDataCollection.py
+++ Control/TcrsDataCollection.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 import logging
+import random
 import numpy
 
 _logger = logging.getLogger(__name__)
 
-SPENT_HOUR = numpy.linspace(0, 8, 17)
-
 
 class WeekActivitiesMatrix(object):
+
+    SPENT_HOUR_OPTIONS = numpy.linspace(0, 8, 17)
 
     def __init__(self, weekday_activities_row_setting, max_hour):
 
@@ -51,13 +52,42 @@ class WeekActivitiesMatrix(object):
         return total_spent_hours
 
     @classmethod
-    def _get_spent_hour(cls, time_range, max_hour, current_spent_hour):
+    def _get_spent_hour(cls, time_range, max_work_hour, current_spent_hour):
 
-        if max_hour - current_spent_hour > 0:
-            pass
+        # format check and decide spent hour
+        split_symbol = '~'
+        if split_symbol in time_range:
+            token = time_range.split(split_symbol)
+            range_start = cls._get_hour_format(token[0])
+            range_end = cls._get_hour_format(token[1])
 
-        if "-" in time_range:
-            pass
+            # spent hour value should choose from SPENT_HOUR_OPTIONS,
+            # so mapping the real hour to list option
+            choose_index_start = range_start * 2
+            choose_index_end = range_end * 2 + 1
+            spent_hour = random.choice(cls.SPENT_HOUR_OPTIONS[choose_index_start:choose_index_end])
+        else:
+            spent_hour = cls._get_hour_format(time_range)
+
+        # check remaining hours
+        if max_work_hour - current_spent_hour + spent_hour >= 0:
+            return spent_hour
+        else:
+            spent_hour = max_work_hour - current_spent_hour
+            spent_hour = cls._get_hour_format(spent_hour)
+            return spent_hour
+
+    @classmethod
+    def _get_hour_format(cls, in_str):
+
+        try:
+            trans_num = float(in_str)
+            assert trans_num in cls.SPENT_HOUR_OPTIONS, "there's no such spent hour option"
+            return trans_num
+
+        except:
+            raise Exception('time range should be like "1.5~3" '
+                            'or exactly a number. e.g. "3".')
 
     @property
     def activity_map(self):
