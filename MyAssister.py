c183f730ae4fe9bceb8bccf33c63424e38c66ef7
alanliu <alanliu@cybersoft4u.com>
2016-12-04 23:29:34 +0800

    Add logger

--- MyAssister.py
+++ MyAssister.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 
-import logging
-
 from Control.TcrsAgent import Agent
+from Utils.LoggingHelper import *
 
-_logger = logging.getLogger(__name__)
+logger = get_logger('./logging.ini')
 
 logging.basicConfig(
     format='[%(asctime)s] %(levelname)s [%(funcName)s] - %(message)s [%(filename)s:%(lineno)d]',
